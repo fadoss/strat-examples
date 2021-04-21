6870487ea7dd37010eebaf30bf4ce37dd4fd4fb1
nobody <nobody@nodomain>
2021-04-21 11:02:54 +0200

    Initial index

---
+++ index.js
@@ -0,0 +1,115 @@
+const exampledb = {r:"https://raw.githubusercontent.com/fadoss/strat-examples/master/",e:[{t:[2],p:[0,1,17]},{t:[4],p:[0]},{t:[0],p:[0]},{f:['ccs-mc.maude','ccs2.maude'],r:'semantics',i:'ccs-mc',t:[0,3],p:[19]},{t:[4],p:[0,1,5]},{f:['bt-labyrinth.maude','backtracking.maude'],r:'params',i:'bt-labyrinth',t:[4],p:[0]},{t:[11,10],p:[0]},{t:[12],p:[0,1,10]},{t:[1],p:[1]},{f:['semtrans.maude','semtrans-test.maude'],r:'metalevel',i:'semtrans',t:[0],p:[3]},{f:['bt-graphs.maude','backtracking.maude'],r:'params',i:'bt-graphs',t:[4],p:[5]},{f:['linprog-analysis.maude','linprog.maude','linprog-examples.maude','linprog-mc.maude'],r:'params/linprog',i:'linprog',t:[4,3],p:[5]},{t:[4,0],p:[5,15]},{t:[4,0],p:[19]},{f:['recfns-mc.maude','recfns.maude'],r:'semantics',i:'recfns-mc',t:[0,3],p:[19]},{t:[3,3],p:[6]},{t:[2],p:[9,10]},{t:[0],p:[9,10]},{f:['bt-queens.maude','backtracking.maude'],r:'params',i:'bt-queens',t:[4],p:[10,17]},{t:[12],p:[12]},{f:['congruenceOpsExt.maude','congruence-lazylist.maude','slangExtension.maude','congruence-foo.maude','slangExtension-iface.maude','congruence-lazylist-smod.maude','slangExtension.py'],r:'metalevel/slangExtension',i:'slangExtension',t:[3],p:[12]},{f:['congruenceOpsExt.maude','slangExtension-iface.maude','slangExtension.maude'],r:'metalevel/slangExtension',i:'congruenceOpsExt',t:[12],p:[12]},{f:['congruence-lazylist.maude','slangExtension-iface.maude','congruenceOpsExt.maude','slangExtension.maude'],r:'metalevel/slangExtension',i:'congruence-lazylist',t:[12],p:[19]},{f:['list-example.maude','multistrat.maude','tictactoe.maude','multistrat.py','multistrat-iface.maude','../stratKit.maude'],r:'metalevel/multistrategies',i:'multistrategies',t:[9,3],p:[12]},{f:['tictactoe.maude','multistrat.maude','../stratKit.maude'],r:'metalevel/multistrategies',i:'tictactoe',t:[2,3],p:[19]},{f:['membranes.py','memrun.maude','memparse.maude','membrane.maude','examples/test_division.memb','examples/ex4.memb','examples/strings.memb','examples/ex1.memb','examples/p53.memb','examples/test_xev.memb','examples/sat_promoters.memb','examples/test_empty.memb','examples/nsquare.memb','examples/test_transitivity.memb','examples/sat.memb','examples/fig3.memb','examples/ex2.memb','examples/divisors.memb','examples/test_cev.memb','examples/ex3.memb'],r:'biology/membranes',i:'membranes',t:[9,8,3],p:[11]},{f:['ANS-completion.maude','N-completion.maude','completion-rules.maude','terese.maude','readme.txt','critical-pairs.maude','S-completion.maude','examples/example-ex7.8.maude','examples/all-completions.maude','examples/example-groups.maude','examples/example-6.2.7.maude','examples/example-7.2.6.maude','examples/example-7.1.1.maude','examples/example-0.maude','examples/all-completions-old.maude','examples/lpo.maude','old/ANS-completion.maude','old/N-completion.maude','old/knuth-bendix.maude','old/S-completion.maude'],r:'deduction/completion',i:'completion',t:[1],p:[17]},{f:['ANS-completion.maude','N-completion.maude','knuth-bendix.maude','S-completion.maude','../critical-pairs.maude'],r:'deduction/completion/old',i:'old',t:[1],p:[4,15]},{f:['normalization.maude','eden-strats.maude','eden.maude','examples.maude'],r:'semantics/eden',i:'eden',t:[0],p:[2]},{t:[2],p:[17]},{t:[2,3],p:[19]},{t:[2],p:[13]},{f:['opsem.maude','opsem.py','../stratKit.maude'],r:'metalevel/opsem',i:'opsem',t:[0,3],p:[19]},{f:['opsem-kleene.maude','opsem.maude','../stratKit.maude'],r:'metalevel/opsem',i:'opsem-kleene',t:[3],p:[19]},{f:['opsem-tlr.maude','opsem.maude','../stratKit.maude'],r:'metalevel/opsem',i:'opsem-tlr',t:[3],p:[19]},{f:['opsem-sync.maude','opsem.maude','../stratKit.maude'],r:'metalevel/opsem',i:'opsem-sync',t:[3],p:[19]},{f:['opsem-river.maude','opsem.maude','../stratKit.maude','../../modelChecking/river-v2.maude'],r:'metalevel/opsem',i:'opsem-river',t:[3],p:[19]},{f:['opsem-cupsballs.maude','opsem.maude','../stratKit.maude','../../games/cupsballs.maude','opsem-kleene.maude'],r:'metalevel/opsem',i:'opsem-cupsballs',t:[3],p:[19]},{f:['opsem-philosophers.maude','opsem-sync.maude','../../modelChecking/philosophers.maude','opsem.maude','../stratKit.maude'],r:'metalevel/opsem',i:'opsem-philosophers',t:[3],p:[19]},{t:[5,2,3],p:[19]},{t:[12],p:[19]},{f:['neuralNetworks.maude','.maude_history','neuralNetworks-data.maude'],r:'biology/neuralNetworks',i:'neuralNetworks',t:[8],p:[14]},{t:[11,10,3],p:[17]},{f:['electoral-system.maude','ambients-types.maude','ambients-types2.maude','ambients-semantics.maude'],r:'semantics/ambients',i:'ambients',t:[0],p:[19]},{t:[12],p:[19]},{t:[2],p:[19]},{t:[1],p:[19]},{t:[12],p:[19]},{t:[12],p:[19]},{t:[3,3],p:[7,15]},{t:[3,3],p:[8]},{t:[2],p:[19]},{f:['river-choice.maude','../modelChecking/river.maude'],r:'probabilistic',i:'river-choice',t:[7,3],p:[19]},{t:[3,3],p:[19]},{t:[3,3],p:[19]},{t:[3,3],p:[7]},{t:[3,3],p:[19]},{t:[6,11,3,3],p:[19]},{t:[6,11,3,3],p:[19]},{f:['wordWrap.maude','wordWrap-examples.maude'],r:'params',i:'wordWrap',t:[4],p:[19]},{f:['fractals.maude','flatMap.maude','fractals.py'],r:'params',i:'fractals',t:[4],p:[19]},{f:['bb-traveler.maude','branchBound-index.maude','bb-traveler-index.maude','bb-traveler-list.maude','branchBound-list.maude','traveler.maude','bb-15puzzle.maude','bb-adapter.maude','branchBound.maude','../../games/15puzzle.maude','../bt-labyrinth.maude','../backtracking.maude'],r:'params/branchBound',i:'branchBound',t:[4],p:[19]},{t:[4],p:[19]},{f:['bb-15puzzle.maude','../../games/15puzzle.maude','branchBound.maude'],r:'params/branchBound',i:'bb-15puzzle',t:[4],p:[19]},{t:[10,7,3],p:[19]},{t:[10,7,3],p:[19]},{f:['ninjas-choice.maude','ninjas-tuple.maude'],r:'probabilistic',i:'ninjas-choice',t:[10,7,3],p:[19]},{t:[10,7],p:[19]},{t:[1],p:[19]},{t:[3,10],p:[19]},{t:[12],p:[19]},{f:['bakery.maude','../metalevel/multistrategies/multistrat.maude','../metalevel/stratKit.maude'],r:'modelChecking',i:'bakery',t:[6,11,3,3],p:[19]},{f:['bt-15puzzle.maude','backtracking.maude','../games/15puzzle.maude'],r:'params',i:'bt-15puzzle',t:[4],p:[19]},{t:[7,3],p:[19]},{t:[11,7,3],p:[19]},{t:[8],p:[19]},{t:[12],p:[19]},{f:['coin.maude','coin.multiquatex'],r:'probabilistic',i:'coin',t:[7,3],p:[15]},{t:[7],p:[19]},{t:[7],p:[19]},{f:['pred-prey.maude','pred-prey-plot.py'],r:'probabilistic',i:'pred-prey',t:[7],p:[19]},{f:['lokta.maude','lokta.quatex'],r:'probabilistic',i:'lokta',t:[7],p:[19]},{t:[7],p:[19]},{t:[7],p:[19]},{f:['crn-base.maude','crn-complete.maude','complete-script.maude','print-module.maude','pred-prey.maude'],r:'metalevel/crn-complete',i:'crn-complete',t:[7],p:[19]},{f:['benchmark.py','maudify.py','runMaude.py','compareAbs.py','templates/crnsim.maude.jinja','templates/crnsim.c.jinja','templates/crnsim.cc.jinja'],r:'probabilistic/crn-generator',i:'crn-generator',t:[7],p:[19]},{f:['brp.maude','brp-okp.multiquatex','brp-timed.multiquatex'],r:'probabilistic',i:'brp',t:[10,11,7,3],p:[19]},{f:['client-server.maude','client-server.multiquatex','pmaude-conf.maude'],r:'probabilistic',i:'client-server',t:[11,7],p:[19]},{f:['symmetric-polling.maude','symmetric-polling.multiquatex','pmaude-conf.maude'],r:'probabilistic',i:'symmetric-polling',t:[11,7],p:[19]},{f:['prob-lang.maude','prob-lang-examples.maude','discarded-filter.py'],r:'probabilistic',i:'prob-lang',t:[0,7],p:[16]},{f:['knuthYao6.maude','knuthYao6.quatex'],r:'probabilistic',i:'knuthYao6',t:[7,3],p:[16]},{t:[7,3],p:[19]},{t:[7],p:[19]},{f:['stpetri-tmpl.maude','stpetri-tmpl.multiquatex'],r:'probabilistic',i:'stpetri-tmpl',t:[7,3],p:[19]},{f:['stpetri-tt.maude','stpetri-tmpl.multiquatex'],r:'probabilistic',i:'stpetri-tt',t:[7,3],p:[19]},{f:['bogosort.maude','bogosort.quatex'],r:'probabilistic',i:'bogosort',t:[7,3],p:[19]},{f:['quic.maude','quic-first.quatex','quic-last.quatex','quic-run-all.py'],r:'probabilistic',i:'quic',t:[7,3],p:[16]},{f:['apmsim.py','pmaude.maude','apmaude.maude','examples/exponential-clock.maude','examples/symmetric-polling.quatex','examples/exponential-clock.quatex','examples/dice.maude','examples/simple-client-server.quatex','examples/simple-client-server.maude','examples/symmetric-polling.maude'],r:'probabilistic/pmaude',i:'pmaude',t:[7,3],p:[18]}],t:13,p:20};
+
+function ZipDownloader(root, name, files) {
+	this.req = new XMLHttpRequest();
+	this.zip = new JSZip();
+	this.root = root;
+	this.name = name;
+	this.files = files;
+	this.hasDepends = files.some(s => s.startsWith('..'));
+	this.index = 0;
+	this.start = function () {
+		this.req.open('GET', this.root + '/' + this.files[0]);
+		this.req.send();
+	};
+	self = this;
+	this.req.addEventListener('readystatechange', function(aevent) {
+		if (self.req.readyState == XMLHttpRequest.DONE) {
+			if (self.req.status == 200) {
+				filename = self.files[self.index];
+				if (filename.indexOf('..') >= 0)
+					filename = filename.substring(filename.lastIndexOf('/') + 1);
+				var content = self.req.responseText;
+				if (self.hasDepends && filename.endsWith('.maude')) {
+					var depth = filename.split('/').length;
+					var regex = new RegExp(`load (../){${depth},}(.*/)*`, 'g');
+					content = content.replaceAll(regex, 'load ');
+				}
+				self.zip.file(filename, content);
+				self.index++;
+				if (self.index == self.files.length)
+					self.zip.generateAsync({type: 'blob'}).then(function(content) {
+						saveAs(content, `${self.name}.zip`);
+					});
+				else {
+					self.req.open('GET', self.root + '/' + self.files[self.index]);
+					self.req.send();
+				}
+			}
+			else
+				console.log('Error while downloading files...');
+		}
+	});
+}
+
+function downloadZip(exnum) {
+	const example = exampledb.e[exnum];
+	if (example.f) {
+		var zipd = new ZipDownloader(exampledb.r + '/' + example.r, example.i, example.f);
+		zipd.start();
+	}
+}
+
+function selectOnly(elnum, istag) {
+	var numitems = istag ? exampledb.t : exampledb.p;
+	var prefix = istag ? 'stag' : 'spub';
+
+	var selected = document.getElementById(`${prefix}${elnum}`);
+
+	if (selected.isonly) {
+		for (var i = 0; i < numitems; i++)
+			document.getElementById(`${prefix}${i}`).checked = true;
+		selected.isonly = false;
+	}
+	else {
+		for (var i = 0; i < numitems; i++)
+			document.getElementById(`${prefix}${i}`).checked = (i == elnum);
+		selected.isonly = true;
+	}
+	applyFilter();
+}
+
+function applyFilter() {
+	const sresults = document.getElementById('search').results;
+
+	for (var i = 0; i < exampledb.e.length; i++) {
+		const example = exampledb.e[i];
+		var visible = false;
+		if (example.t) {
+			for (var tag of example.t) {
+				if (document.getElementById(`stag${tag}`)?.checked) {
+					visible = true;
+					break;
+				}
+			}
+		}
+		if (visible && example.p) {
+			visible = false;
+			for (var pub of example.p) {
+				if (document.getElementById(`spub${pub}`)?.checked) {
+					visible = true;
+					break;
+				}
+			}
+		}
+		if (visible && sresults) {
+			visible = false;
+			for (var res of sresults) {
+				if (res.ref == i) {
+					visible = true;
+					break;
+				}
+			}
+		}
+		document.getElementById(`exm${i}`).style.display = visible ? 'block' : 'none';
+	}
+}
+
+function doSearch() {
+	var sfield = document.getElementById('search');
+	if (sfield.value)
+		sfield.results = searchdb.search(sfield.value)
+	else
+		sfield.results = null;
+	applyFilter();
+}
